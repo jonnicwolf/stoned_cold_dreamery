e64974fbc29732c25712bfdc55b6a8a4a90d4e3b
Fezz <jonnicwolf@gmail.com>
2023-07-11 17:42:39 -0400

    cleanup

--- src/components/NewsLetterSignUp.jsx
+++ src/components/NewsLetterSignUp.jsx
@@ -1,46 +1,73 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
-const cone = require('/Users/jonathannarine/GithubRepos/cold_stoned_dreamery/src/photo_assets/logos_banners/cone_whiteBorder.jpg')
+const cone = require('/Users/jonathannarine/GithubRepos/cold_stoned_dreamery/src/photo_assets/logos_banners/cone_whiteBorder.jpg');
+
 const NewsLetterSignUp = () => {
-  const cones = []
-  for (let i=0; i<=223; i++) {
+  const cones = [];
+  for (let i=0; i<=44; i++) {
     cones.push(<Cone src={cone} alt=''/>)
-  }
+  };
   return (
     <>
       <Container>
         {cones}
-        <form action="">
-
-        </form>
       </Container>
-      <Title>GET NEWS & UPDATES</Title>
+      <InputContainer>
+        <Title>GET NEWS & UPDATES</Title>
+        <Form action="">
+          <Input type="text" />
+          <Button>SIGN<br/>UP</Button>
+        </Form>
+      </InputContainer>
     </>
   );
 };
 
+const Button = styled.button`
+  border: 0;
+  background-color: white;
+  font-size: 30px;
+  height: 100px;
+  padding: 0;
+  transform: translateY(-15px);
+  width: 150px;
+`;
 const Container = styled.div`
   background-color: white;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
   gap: 15px;
-  height: 500px;
+  height: 450px;
   margin-bottom: 10px;
-  padding-left: 0px;
   padding-top: 20px;
   width: 100%;
 `;
 const Cone = styled.img`
-  width: 30px;
-  height: 50px;
+  width: 80px;
+  height: 140px;
   opacity: 20%
 `;
+const Form = styled.form`
+  background-color: white;
+`;
+const Input = styled.input`
+  border: none;
+  border-right-style: solid;
+  border-right-color: grey;
+  border-right-width: 1px;
+  height: 150px;
+  padding: 10px;
+  width: 400px;
+`;
+const InputContainer = styled.div`
+  transform: translateY(-400px);
+`;
 const Title = styled.h1`
-  color: white;
-  top: 50%;
-  left: 50%;
-`
+  color: black;
+  transform: translateX(15px);
+  font-size: 50px;
+`;
 
 export default NewsLetterSignUp;
