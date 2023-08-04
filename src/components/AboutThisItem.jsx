be71e873ab70c7b0609056c22fb6b58de75dd8b7
Fezz <jonnicwolf@gmail.com>
2023-08-04 10:34:02 -0400

    aboutthisitem scaffolding

---
+++ src/components/AboutThisItem.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const AboutThisItem = () => {
+  return (
+    <div>AboutThisItem</div>
+  );
+};
+
+export default AboutThisItem;
