ba6719a1014f37a28b385e7800f424405e4be21d
Fezz <jonnicwolf@gmail.com>
2023-08-24 09:30:06 -0400

    more distraction

--- src/components/NewsLetterSignUp.jsx
+++ src/components/NewsLetterSignUp.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 const cone = require('../photo_assets/logos_banners/cone_whiteBorder.png')
 
 const NewsLetterSignUp = () => {
   const clientWidth = document.documentElement.clientWidth;
-  console.log(clientWidth)
   const cones = [];
-  for (let i=0; i<=85; i++) {
+
+  for (let i=0; i<=670; i++) {
     cones.push(<Cone src={cone} alt=''/>)
   };
   if (clientWidth === 390) cones.length = 12;
 
   return (
     <Container>
-
 
       <InputContainer>
         <Title>GET NEWS & UPDATES</Title>
@@ -27,6 +25,7 @@ const NewsLetterSignUp = () => {
           By clicking 'SIGN UP,' I represent I am at least the age of twenty-one (21), I have read, understood, and agree to the Terms of Service and Privacy Policy and I agree to receive emails and customer service communications from Houseplant via email. I can unsubscribe at any time by clicking 'unsubscribe' in emails.
         </Disclaimer>
       </InputContainer>
+
       <ConeContainer> {cones} </ConeContainer>
 
     </Container>
@@ -48,31 +47,27 @@ const Button = styled.button`
   font-family: var(--font-family-modak);
   height: 60px;
   padding: 0;
-  // transform: translateY(-5px);
-  // @media (min-width: 390px) {
-  //   font-size: 20px;
-  //   transform: translateX(50px);
-  //   width: 30vw;
-  // }
-  // @media (min-width: 768px) {
-  //   font-size: 30px;
-  //   transform: translateX(19px);
-  //    width: 20vw;
-  // }
-  // @media (min-width: 1024px) {
-  //   font-size: 35px;
-  //   width: 15vw;
-  //   transform: translateX(83px);
-  // }
-  // @media (min-width: 1440px) {
-  //   border: 1px solid red;
-  //   transform: translateX(19px);
-  //   width: 10vw;
-  // }
-  // @media (min-width: 2560px) {
-  //   font-size: 40px;
-  //   width: 7vw;
-  // }
+  transform: translateY(-5px);
+  @media (min-width: 390px) {
+    font-size: 18px;
+    transform: translateX(0.1px);
+    width: 20vw;
+  }
+  @media (min-width: 768px) {
+    font-size: 25px;
+     width: 18vw;
+  }
+  @media (min-width: 1024px) {
+    font-size: 35px;
+    width: 15vw;
+  }
+  @media (min-width: 1440px) {
+    width: 10vw;
+  }
+  @media (min-width: 2560px) {
+    font-size: 40px;
+    width: 7vw;
+  }
 `;
 const ConeContainer = styled.div`
   background-color: white;
@@ -80,55 +75,57 @@ const ConeContainer = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   gap: 5px;
-  height: 42vh;
+  height: 29vh;
   overflow: hidden;
   padding-top: 10px;
   padding-bottom: 10px;
+  position: relative;
   width: 100vw;
   z-index: 1;
-  position: relative;
+  @media (min-width: 768px) {
+    height: 33vh;
+  }
+  @media (min-width: 1024px) {
+    height: 39vh;
+    gap: 10px;
+  }
 `;
 const Cone = styled.img`
-  height: 10vh;
-  width: 5vw;
-  opacity: 20%
+  height: 3vh;
+  width: 3vw;
+  opacity: 20%;
 `;
 const Disclaimer = styled.div`
   font-color:black;
   font-size: 12px;
   padding-top: 10px;
   width: 50vw;
-  // @media (min-width: 390px) {
-  //   transform: translateX(35px);
-  //   width: 90vw;
-  // }
-  // @media (min-width: 1024px) {
-  //   transform: translateX(70px);
-  // }
-  // @media (min-width: 2560px) {
-  //   width: 40vw;
-  //   transform: translateX(700px);
-  // }
-`
+  @media (min-width: 390px) {
+    font-size: 10px;
+    width: 80vw;
+  }
+  @media (min-width: 2560px) {
+    width: 40vw;
+  }
+`;
 const Form = styled.form`
   background-color: white;
   border: 3px inset aqua;
   display: flex;
-  // justify-content: center;
-  // align-items: center;
   width: 50vw;
-  // @media (min-width: 768px) {
-  //   width: 50%;
-  //   // transform: translateX(20px);
-  // }
-  // @media (min-width: 1024px) {
-  //   // width: 95%;
-  //   // transform: translateX(30px);
-  // }
-  // @media (min-width: 2560px) {
-  //   // width: 50vw;
-  //   // transform: translateX(590px);
-  // }
+  @media (min-width: 424px) {
+    width: 80vw;
+  }
+  @media (min-width: 768px) {
+    width: 90vw;
+  }
+  @media (min-width: 1024px) {
+    width: 90vw;
+    
+  }
+  @media (min-width: 2560px) {
+    width: 50vw;
+  }
 `;
 const Input = styled.input`
   border: none;
@@ -136,25 +133,24 @@ const Input = styled.input`
   outline: none;
   padding: 10px;
   width: 42vw;
-  // @media (min-width: 390px) {
-  //   // width: 50vw;
-  //   font-size: 22px;
-  // }
-  // @media (min-width: 768px) {
-  //   // width: 70vw;
-  //   font-size: 28px;
-  // }
-  // @media (min-width: 1024px) {
-  //   font-size: 35px;
-  // }
-  // @media (min-width: 1440px) {
-  //   border: 1px solid red;
-  //   // width: 82.3vw;
-  // }
-  // @media (min-width: 2560px) {
-  //   width: 41.5vw;
-  //   font-size: 45px;
-  // }
+  @media (min-width: 390px) {
+    width: 56vw;
+    font-size: 22px;
+  }
+  @media (min-width: 768px) {
+    width: 70vw;
+    font-size: 28px;
+  }
+  @media (min-width: 1024px) {
+    font-size: 35px;
+  }
+  @media (min-width: 1440px) {
+    width: 82.3vw;
+  }
+  @media (min-width: 2560px) {
+    width: 41.5vw;
+    font-size: 45px;
+  }
 `;
 const InputContainer = styled.div`
   display: flex;
@@ -163,30 +159,19 @@ const InputContainer = styled.div`
   position: relative;
   z-index: 2;
   height: 0.1vh;
-  // transform: translateY(-400px);
 `;
 const Title = styled.h1`
   color: black;
-  // transform: translateX(20px);
   font-size: 50px;
-  // @media (min-width: 390px) {
-  //   font-size: 30px;
-  //   transform: translateX(50px);
-  // }
-  // @media (min-width: 768px) {
-  //   font-size: 50px;
-  //   transform: translateX(110px);
-  // }
-  // @media (min-width: 1024px) {
-  //   transform: translateX(250px);
-  // }
-  // @media (min-width: 1440px) {
-  //   transform: translateX(450px);
-  // }
-  // @media (min-width: 2560px) {
-  //   font-size: 60px;
-  //   transform: translateX(900px);
-  // }
+  @media (min-width: 390px) {
+    font-size: 30px;
+  }
+  @media (min-width: 768px) {
+    font-size: 50px;
+  }
+  @media (min-width: 2560px) {
+    font-size: 60px;
+  }
 `;
 
 export default NewsLetterSignUp;
