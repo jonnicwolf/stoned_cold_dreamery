c3456b03ad3fa3bc170e8d26a8672d696b401f55
Fezz <jonnicwolf@gmail.com>
2023-08-31 13:56:29 -0400

    adds checkoutpage

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { PaymentElement } from '@stripe/react-stripe-js';
+
+const CheckoutPage = () => {
+  return (
+    <PaymentElement />
+  );
+};
+
+export default CheckoutPage;
