8f5954161b10c0a03bcb3652bec6fcc43deed445
Fezz <jonnicwolf@gmail.com>
2023-09-26 15:15:11 -0400

    deploys herbpage

--- src/pages/HerbPage.jsx
+++ src/pages/HerbPage.jsx
@@ -1,30 +1,63 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import NutritionSummary from '../components/NutritionSummary';
-import NutritionFacts from '../components/NutritionFacts';
-import Ingredients from '../components/Ingredients';
 import AddItemToCart from '../components/AddItemToCart';
-import AboutThisItem from '../components/AboutThisItem';
 
 const HerbPage = () => {
-  const [flavor,setFlavor] = useState('birthday_cake');
-  const { cream } = require('../data.json');
+  const [flavor,setFlavor] = useState('indica');
+  const { smokable } = require('../data.json');
+
+  function handleFlavor (event) {
+    setFlavor(event.target.value);
+  };
+
   return (
     <Container>
       <ImgContainer>
-        <Img />
+        <Img src={ smokable[flavor].photo } alt={ smokable[flavor].name }/>
       </ImgContainer>
+
+      <Segment>
+        <ProductTitle>
+          <FlavorDropList value={flavor} onChange={handleFlavor}>
+            <Option value="indica">King Louis</Option>
+            <Option value="sativa">Blue Dream</Option>
+            <Option value="hybrid">Ovan</Option>
+          </FlavorDropList>
+          <P>{smokable[flavor].description}</P>
+        </ProductTitle>
+      </Segment>
+
+      <Segment>
+        <AddItemToCart item={flavor} itemType={'smokable'} />
+      </Segment>
     </Container>
   );
 };
 
 const Container = styled.div`
-  align-items: flex-start;
+  align-items: center;
   display: flex;
   height: 100%;
   gap: 30px;
   padding: 2vw;
   margin-top: 10vh;
+`;
+const FlavorDropList = styled.select`
+  border: none;
+  font-family: 'Permanent Marker', cursive;
+  font-weight: normal;
+  font-size: 36px;
+  margin: 0;
+  outline: none;
+  &:active {
+    border: none;
+  }
+`;
+const Segment = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30vw;
+  height: 100%;
 `;
 const ImgContainer = styled(Segment)`
   overflow: hidden;
@@ -33,6 +66,16 @@ const ImgContainer = styled(Segment)`
 const Img = styled.img`
   width: 60vw;
   transform: translate(-125px, -80px);
+`;
+const Option = styled.option`
+  font-size: 10px;
+`;
+const P = styled.p`
+  font-family: 'Chakra Petch', sans-serif;
+  margin: 0;
+`;
+const ProductTitle = styled.div`
+  margin-bottom: 2vh;
 `;
 
 export default HerbPage;
