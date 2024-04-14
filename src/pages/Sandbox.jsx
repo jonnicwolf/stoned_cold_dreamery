caec14b4d648ab77dd888524345514fd32bec994
Fezz <jonnicwolf@gmail.com>
2024-04-14 16:26:38 -0400

    ci fixes

--- src/pages/Sandbox.jsx
+++ src/pages/Sandbox.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import styled from 'styled-components';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 
 const Sandbox = () => {
   return (
