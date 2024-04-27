79971ac984954fe2f2762b5a1d7f7c59c0bef27a
Fezz <jonnicwolf@gmail.com>
2024-04-27 05:31:49 -0400

    adds Boricanna and some scaffolding

---
+++ src/components/Boricanna.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Boricanna = () => {
+  return (
+    <div>Boricanna</div>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 80vw;
+  width: 100vw;
+`;
+
+const CarouselBox = styled.div`
+  height: 35%;
+  width: 100%;
+`;
+const TextBox = styled.div`
+  height: 45%;
+  width: 100%;
+`;
+
+export default Boricanna;
